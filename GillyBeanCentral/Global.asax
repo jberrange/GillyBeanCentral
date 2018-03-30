90d2df644d39cf3cffd2918afdb21dd1243f80dc
James Berrange <james.berrange@gmail.com>
2018-03-30 16:06:53 +1300

    Add project files.

---
+++ GillyBeanCentral/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GillyBeanCentral.MvcApplication" Language="C#" %>
---
+++ GillyBeanCentral/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GillyBeanCentral.Startup))]
+namespace GillyBeanCentral
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
