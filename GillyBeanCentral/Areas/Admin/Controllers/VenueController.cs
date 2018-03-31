5494efc0d750789267bbae819437190b9012d6c1
James Berrange <james.berrange@gmail.com>
2018-03-31 14:10:42 +1300

    Added some new fields to venue changes manually and seems to be working.

---
+++ GillyBeanCentral/Areas/Admin/Controllers/VenueController.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using GillyBeanCentral.Entities;
+using GillyBeanCentral.Models;
+
+namespace GillyBeanCentral.Areas.Admin.Controllers
+{
+    public class VenueController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Admin/Venue
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.Venues.ToListAsync());
+        }
+
+        // GET: Admin/Venue/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Venue venue = await db.Venues.FindAsync(id);
+            if (venue == null)
+            {
+                return HttpNotFound();
+            }
+            return View(venue);
+        }
+
+        // GET: Admin/Venue/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/Venue/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "Id,VenueName,Byline,HasAccommodation, HasEventFacilities, ThumbnailUrl, Url,VenueTypeID")] Venue venue)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Venues.Add(venue);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(venue);
+        }
+
+        // GET: Admin/Venue/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Venue venue = await db.Venues.FindAsync(id);
+            if (venue == null)
+            {
+                return HttpNotFound();
+            }
+            return View(venue);
+        }
+
+        // POST: Admin/Venue/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "Id,VenueName,Byline,HasAccommodation, HasEventFacilities, ThumbnailUrl, Url,VenueTypeID")] Venue venue)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(venue).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(venue);
+        }
+
+        // GET: Admin/Venue/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Venue venue = await db.Venues.FindAsync(id);
+            if (venue == null)
+            {
+                return HttpNotFound();
+            }
+            return View(venue);
+        }
+
+        // POST: Admin/Venue/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            Venue venue = await db.Venues.FindAsync(id);
+            db.Venues.Remove(venue);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
--- GillyBeanCentral/Entities/Venue.cs
+++ GillyBeanCentral/Entities/Venue.cs
@@ -22,9 +22,17 @@ namespace GillyBeanCentral.Entities
         [Required]
         public string Byline { get; set; }
 
+        public bool HasAccommodation { get; set; }
+
+        public bool HasEventFacilities { get; set; }
+
+
         [MaxLength(1024)]
         public string Url { get; set; }
 
+        [MaxLength(1024)]
+        public string ThumbnailUrl { get; set; }
+        
         public int VenueTypeID { get; set; }
 
     }
